cda7d34b77e6ad00c63251744c6fc59079d28b5a
ishellstrike <ishellstrike@gmail.com>
2016-02-20 03:04:09 +0300

    memset

---
+++ World/tools/order_casters.h
@@ -0,0 +1,9 @@
+#pragma once
+#ifndef ORDER_CASTERS_H_
+#define ORDER_CASTERS_H_
+#include <string>
+#include <sstream>
+std::string GetMemoryString(size_t workset);
+
+std::string GetSpeedString(size_t workset);
+#endif //ORDER_CASTERS_H_
