5964fdb273197b6b3dd5b0bdde32cc9e11afe456
ishellstrike <ishellstrike@gmail.com>
2016-03-03 21:49:21 +0300

    watertess stub

---
+++ World/Core/WaterBlockTesselator.h
@@ -0,0 +1,31 @@
+// ============================================================================
+// ==         Copyright (c) 2016, Samsonov Andrey and Smirnov Denis          ==
+// ==                  See license.txt for more information                  ==
+// ============================================================================
+#pragma once
+#include "MeshBlockGenerator.h"
+#include "BlockTessellator.h"
+#include "..\Render\Model.h"
+#include "TessellatorParams.h"
+
+/// Генератор модели для блока.
+/// Отсекает невидимые стороны блока.
+class WaterBlockTessellator : public BlockTessellator
+{
+public:
+  WaterBlockTessellator();
+  ~WaterBlockTessellator() = default;
+
+  void JsonLoad(const rapidjson::Value &val) override;
+
+  PGameObjectTessellator Clone() override;
+
+  const Model &GetModel(const TessellatorParams &params) override;
+
+private:
+  Model mModel;
+  MeshBlockGenerator mGenerator;
+  float percent = 1;
+};
+
+REGISTER_BLOCK_TESSELLATOR(WaterBlockTessellator)
